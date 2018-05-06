2a4cd2e998fbbc604fb0e2c9cdeb7f8f37343979
mroz <private@mrozo.pl>
2018-05-06 18:41:44 +0200

    code can now connect to the server and read the response

--- device/ESP8266_RFID/ESP8266_RFID.ino
+++ device/ESP8266_RFID/ESP8266_RFID.ino
@@ -1,152 +1,130 @@
-//#include <ESP8266WiFi.h>
 #include <SoftwareSerial.h>
+#include <Arduino.h>
 
-SoftwareSerial RFIDSerial(2, 10); // RX, TX
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
 
-int flag=0,RX_Flag=0;//serial port sign
-char Code[14]; 
-long Num=0;//decode data
-int serpin=0;//pin of servo
-int Door=0;
+#include <ESP8266HTTPClient.h>
 
-const char* ssid     = "hackerspace_publiczny";
-const char* password = "przyjmujemy_datki_x86";
+#define DEBUG 
 
-const char* host = "192.168.88.246";
-const int httpPort = 5000;
-char rx_byte = 0;
-String rx_str = "";
+#ifdef DEBUG
+#define D(msg) Serial.println(msg)
+#else
+#define D(msg) ((void)0)
+#endif
 
-void setup() {
-  Serial.begin(115200);
-  RFIDSerial.begin(9600);
-}
 
-void loop() {
-  int i;
-  long temp = 0, time = 0;
-  RX_Flag = 0;
-  while (1){
-    Read_ID();
-    if (RX_Flag == 1)
-    {
-      for (i = 5; i < 11; i++) //
-      {
-        Num <<= 4;
-        if (Code[i] > 64)  Num += ((Code[i]) - 55);
-        else Num += ((Code[i]) - 48);
-        RFIDSerial.println(Num);
-      }
-      RFIDSerial.println(Num);
-//      if ((Num == Password1) || (Num == Password2)) //识别成功
-//      {
-//        for (int i = 0; i <= 50; i++) //
-//        {
-//          pulse(serpin, 180); //
-//        }
-//        delay(2000);
-//        for (int i = 0; i <= 50; i++) //
-//        {
-//          pulse(serpin, 20); //
-//        }
-//      }
-      while (RFIDSerial.read() > 0); //
-      RX_Flag = 0; //
-      Num = 0; //
-    }
+#define URL "http://192.168.88.4:5000/?card="
+//#define URL "http://192.168.122.1:80/?card="
+
+
+SoftwareSerial RFIDSerial(12, 10); // RX, TX
+ESP8266WiFiMulti WiFiMulti;
+
+void id2str(int id[14], char strid[64]){
+  int stridoffset = 0;
+  memset(strid, 0,  64);
+  for(int offset=0; offset < 14; offset++){
+    itoa(id[offset], strid + stridoffset, 10);
+    stridoffset += strlen(strid + stridoffset);
+    strid[stridoffset]=',';
+    stridoffset++;
   }
 }
 
-//
-//String Read_database() {
-//  rx_str = "";
-//  //  WiFi.forceSleepWake();
-//  delay(10);
-//  Serial.print("Connecting to ");
-//  Serial.println(ssid);
-//  WiFi.begin(ssid, password);
-//  while (WiFi.status() != WL_CONNECTED) {
-//    delay(500);
-//    Serial.print(".");
-//  }
-//
-//  Serial.println("");
-//  Serial.println("WiFi connected");
-//  Serial.print("connecting to ");
-//  Serial.println(host);
-//
-//  // Use WiFiClient class to create TCP connections
-//  WiFiClient client;
-//  if (!client.connect(host, httpPort)) {
-//    Serial.println("connection failed");
-//  }
-//
-//  //   We now create a URI for the request
-//  String url = "/cards";
-//  Serial.print("Requesting URL: ");
-//  Serial.println(url);
-//
-//  // This will send the request to the server
-//  client.print(String("GET ") +
-//               url + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" + "Accept: text/html" + "\r\n" + "Connection: close\r\n\r\n");
-//  delay(10);
-//
-//  // Read all the lines of the reply from server and print them to Serial
-//  Serial.println("Respond:");
-//  while (client.available()) {
-//    String readInfo = client.readStringUntil('\r');
-//    Serial.print(readInfo);
-//
-//  }
-//
-//  Serial.println();
-//  Serial.println("closing connection");
-//  //  stopWiFiAndSleep();
-//}
 
-void Read_ID(void)
-{
-    int i=0;
-    char temp;
-    for(i=0;(RFIDSerial.available()>0);i++)//
-    //while(Serial.available()>0)
-    {
-      temp=RFIDSerial.read();
-      RFIDSerial.print(temp);
-      delay(2);
-      RFIDSerial.println(i);
-      if(temp==0X02)  //recieve the ssrt bit
-        {
-         flag=1;i=0;RX_Flag=0;//
-        }
-      if(flag==1)//detect the start bit and recieve data
-      {
-        if(temp==0X03)//detect the end code,
-        {
-         flag=0;  //zero clearing
-         if(i==13) RX_Flag=1;//
-         else RX_Flag=0;
-       break;
-        }
-         Code[i]=temp;
-      } 
-    }       
-     flag=0;//
+char queryUrl[128] = URL;
+HTTPClient http;
+
+int digit;
+int id[14];
+int offset=0;
+int state=0;
+char* strid = queryUrl + sizeof(URL) - 1;
+
+void setup() {
+  Serial.begin(115200);
+  D("setup:rfid");
+  RFIDSerial.begin(9600);
+  D("setup:wifi");
+  WiFiMulti.addAP("Hackerspace:AP", "dupaDupa42");
+  D("setup:connecting to wifi");
+  while(WiFiMulti.run() != WL_CONNECTED){delay(100);D(".");}
+  D("setup:end");
+
 }
 
-//void stopWiFiAndSleep() {
-//  WiFi.disconnect();
-//  WiFi.mode(WIFI_OFF);
-//  WiFi.forceSleepBegin();
-//  delay(1);
-//}
+bool sendQuery(){
+  D("sendQuery");
+  D(queryUrl);
+  http.begin(queryUrl);
+  int httpCode = http.GET();
 
-void pulse( int serpin,int angle)
-{
-  int width;//
-  width=(angle*11)+500;//
-  digitalWrite(serpin,HIGH);//
-  delayMicroseconds(width);//
-  digitalWrite(serpin,LOW);//
-  delay(20-width/1000);
+  if(httpCode > 0) {
+            // HTTP header has been send and Server response header has been handled
+            Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+
+            // file found at server
+            if(httpCode == HTTP_CODE_OK) {
+                String payload = http.getString();
+                return strcmp("yes", payload) ==0;
+            }
+        } else {
+            Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+        }
+
+        http.end();
+        return false;
 }
+
+
+void loop() {
+
+  switch(state){
+
+    case 0:
+      if(RFIDSerial.available()){
+        id[offset] = RFIDSerial.read();
+        if(offset==0 && id[offset] !=2){
+          //wait for start
+          break;
+        }
+        offset++;
+        if(offset == 14){
+          state=1;
+        }
+      }
+      break;
+    case 1:
+      id2str(id, stri
+      break;d);
+      D(strid);
+      state=2;
+      offset=0;
+      break;
+    case 2:
+      if(sendQuery()){
+        state=3;
+      } else {
+        state=0; 
+      }
+      break;
+    case 3:
+      //set servo to open
+      //wait 1000
+      //disable servo
+      //wait 5000
+      //set servo to close
+      //wait 1000
+      //disable servo
+      
+      break;
+    default:
+      D("ERR");
+      
+      
+  }
+  
+}
+
